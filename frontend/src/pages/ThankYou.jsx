600bfda01ee7fc914ed1f6d58e2494428de8c7fe
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2024-01-09 21:33:56 +0530

    Updated Thank you page

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -1,26 +1,69 @@
-import React from 'react'
-import { Container, Row, Col, Button } from 'reactstrap'
-import { Link } from 'react-router-dom'
-import '../styles/thank-you.css'
+import React, { useRef, useState } from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/thank-you.css";
+import { useSearchParams } from "react-router-dom";
+import { FaCopy } from "react-icons/fa";
+
+const CopyToClipboard = ({ referenceNum }) => {
+  const textAreaRef = useRef(null);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    textAreaRef.current.select();
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <span>
+      <textarea
+        ref={textAreaRef}
+        value={referenceNum}
+        readOnly
+        style={{ position: "absolute", left: "-9999px" }}
+      />
+      <span className="copy-icon" onClick={copyToClipboard}>
+        {isCopied ? "Copied!" : <FaCopy />}
+      </span>
+    </span>
+  );
+};
 
 const ThankYou = () => {
-   return (
-      <section>
-         <Container>
-            <Row>
-               <Col lg='12' className='pt-5 text-center'>
-                  <div className="thank__you">
-                     <span><i class='ri-checkbox-circle-line'></i></span>
-                     <h1 className='mb-3 fw-semibold'>Thank You</h1>
-                     <h3 className='mb-4'>Your Tour Is Bookked</h3>
+  const searchQuery = useSearchParams()[0];
+  const referenceNum = searchQuery.get("reference");
 
-                     <Button className='btn primary__btn w-25'><Link to='/home'>Back To Home</Link></Button>
-                  </div>
-               </Col>
-            </Row>
-         </Container>
-      </section>
-   )
-}
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12" className="pt-5 text-center">
+            <div className="thank__you">
+              <span>
+                <i className="ri-checkbox-circle-line"></i>
+              </span>
+              <h1 className="mb-3 fw-semibold">Thank You</h1>
+              <h3 className="mb-4">Your Tour Is Booked</h3>
+              <h6 className="mb-4"> 
+                Save this payment Id for future reference :{""}
+                <CopyToClipboard referenceNum={referenceNum} />
+              </h6>
 
-export default ThankYou
+              <Button className="btn primary__btn w-25">
+                <Link to="/home">Back To Home</Link>
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default ThankYou;
