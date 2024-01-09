93d06b3d174e8fe65da56ec0b57edc83b7c7a438
UPADHYAY-YATENDRA <yupadhyayyk@gmail.com>
2024-01-09 18:16:33 +0530

    Updated Payemnt Verfication

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -27,46 +27,46 @@ export const checkout = async (req, res) => {
     });
   }
 };
-
 export const paymentVerification = async (req, res) => {
-  try {
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-      req.body;
-
-    const body = razorpay_order_id + "|" + razorpay_payment_id;
-
-    const expectedSignature = crypto
-      .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
-      .update(body.toString())
-      .digest("hex");
-
-    const isAuthentic = expectedSignature === razorpay_signature;
-
-    if (isAuthentic) {
-      // Database operation comes here
-      await Payment.create({
-        razorpay_order_id,
-        razorpay_payment_id,
-        razorpay_signature,
-      });
-
-      res.redirect(
-        `https://travel-world-drab.vercel.app//thank-you?reference=${razorpay_payment_id}`
-      );
-    } else {
-      res.status(400).json({
+    try {
+      const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+        req.body;
+  
+      const body = razorpay_order_id + "|" + razorpay_payment_id;
+  
+      const expectedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+        .update(Buffer.from(body))
+        .digest("hex");
+  
+      const isAuthentic = expectedSignature === razorpay_signature;
+  
+      if (isAuthentic) {
+        // Database operation comes here
+        await Payment.create({
+          razorpay_order_id,
+          razorpay_payment_id,
+          razorpay_signature,
+        });
+  
+        res.redirect(
+          `https://travel-world-drab.vercel.app//thank-you?reference=${razorpay_payment_id}`
+        );
+      } else {
+        res.status(400).json({
+          success: false,
+          error: "Invalid Signature",
+        });
+      }
+    } catch (error) {
+      console.error("Error during payment verification:", error);
+      res.status(500).json({
         success: false,
-        error: "Invalid Signature",
+        error: "Internal Server Error",  
       });
     }
-  } catch (error) {
-    console.error("Error during payment verification:", error);
-    res.status(500).json({
-      success: false,
-      error: "Internal Server Error",  
-    });
-  }
-};
+  };
+  
 
 export const getKey = (req, res) => {
   try {
